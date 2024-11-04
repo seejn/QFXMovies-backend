aa9a5d6a317eb8579eef7399036f3a97796c8686
seejn <seejnmaharjan@gmail.com>
2024-11-04 13:47:22 +0545

    created method for seeding data

---
+++ src/utils/seed.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+
+import connectDB from "../config/database.js";
+
+dotenv.config();
+const seed = async (model, data) => {
+    await connectDB();
+    await model.deleteMany({})
+    
+    try{
+        await model.insertMany(data)
+    
+        console.log("Seed Completed successfully");
+        process.exit(0);
+    }catch(error){
+        console.log("error seeding...");
+        process.exit(1);
+    }
+}
+
+export default seed;
