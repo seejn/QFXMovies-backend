07aee86765bc61e9bcc6557031b929db75711e5a
seejn <seejnmaharjan@gmail.com>
2024-11-04 19:59:09 +0545

    modified cinema services

--- src/services/cinemaServices.js
+++ src/services/cinemaServices.js
@@ -1,32 +1,21 @@
 import Cinema from "../models/Cinema.js";
 
 export const getAllCinemas = async () => {
-    const cinemas = await Cinema.find();
-    return cinemas;
+    return await Cinema.find().populate("locatedIn");
 }
 
 export const getCinemaById = async (id) => {
-    const cinema = await Cinema.findOne({"_id": id});
-    return cinema;
+    return await Cinema.findOne({"_id": id}).populate("locatedIn");
 }
 
 export const addCinema = async (data) => {
-    const createdCinema = await Cinema.create(data)
-    return createdCinema;
+    return await Cinema.create(data);
 }
 
 export const updateCinema = async (id, data) => {
-    try{
-        return await Cinema.findOneAndUpdate({"_id": id}, data, {new: true});
-    }catch(error){
-        console.log("From service", error);
-    }
+    return await Cinema.findOneAndUpdate({"_id": id}, data, {new: true});
 }
 
 export const deleteCinema = async (id) => {
-    try{
-        return await Cinema.deleteOne({"_id": id});
-    }catch(error){
-        console.log("From Cinema Services", error);
-    }
+    return await Cinema.findOneAndDelete({"_id": id});
 }
