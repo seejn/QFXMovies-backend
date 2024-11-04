c802ffd04f910ea7369f66ae84d2933efd208be8
seejn <seejnmaharjan@gmail.com>
2024-11-04 19:59:46 +0545

    created module for seeding movie data

--- src/seed/movie.js
+++ src/seed/movie.js
@@ -1,9 +1,5 @@
-import dotenv from "dotenv";
-
 import Movie from "../models/Movie.js";
-import connectDB from "../config/database.js";
-
-dotenv.config();
+import seed from "../utils/seed.js";
 
 const movies = [
     {
@@ -22,23 +18,4 @@ const movies = [
         name: "Gharjwain",
     },
 ];
-
-const seedMovies = async () => {
-    await connectDB();
-    await Movie.deleteMany({})
-    
-    try{
-        const moviePromise = movies.map(movie => {
-            console.log(movie)
-            return Movie.create(movie);
-        });
-        await Promise.all(moviePromise);
-    
-        console.log("Movies seeded");
-        process.exit(0);
-    }catch(error){
-        console.log("error seeding movies");
-    }
-}
-
-seedMovies();
+seed(Movie, movies);
