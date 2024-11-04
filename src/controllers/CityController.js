b176f7c0e68dc8f01c834f4a8b2e91f39e9960a7
seejn <seejnmaharjan@gmail.com>
2024-11-04 13:38:05 +0545

    added "getCity, createCity, removeCity, updateCity" methods in CityController and CityServices

--- src/controllers/CityController.js
+++ src/controllers/CityController.js
@@ -4,3 +4,46 @@ export const getAllCities = async (req, res) => {
     const data = await cityService.getAllCities();
     res.status(200).json(data.map(d => ({"id": d._id, "name": d.name})));
 }
+
+export const getCity = async(req, res) => {
+    const {id} = req.params;
+    try{
+        const data = await cityService.getCity(id);
+        res.status(200).json(data);
+    }catch(error){
+        console.log("Error while getting single city", error)
+        res.status(400);
+    }
+}
+export const createCity = async(req, res) => {
+    try{
+        const data = await cityService.createCity(req.body);
+        res.status(200).json({message: "New city added successfully", data: data});
+    }catch(error){
+        console.log("Error while create single city", error)
+        res.status(400);
+    }
+}
+export const removeCity = async(req, res) => {
+    const {id} = req.params;
+    try{
+        const data = await cityService.removeCity(id);
+        res.status(200).json({message: "City removed successfully", data: data});
+    }catch(error){
+        console.log("Error while getting single city", error)
+        res.status(400);
+    }
+}
+export const updateCity = async(req, res) => {
+    const {id} = req.params;
+    try{
+        const data = await cityService.updateCity(id, req.body);
+        res.status(200).json({message: "City updated successfully", data: data});
+    }catch(error){
+        console.log("Error while updating city", error);
+        if(error.code === 11000 && error.codeName === "DuplicateKey"){
+            return res.status(400).json({message: "City already exists."});
+        }
+        res.status(400).json({message: "something went wrong"});
+    }
+}
--- src/services/cityServices.js
+++ src/services/cityServices.js
@@ -3,3 +3,19 @@ import City from '../models/City.js';
 export const getAllCities = async () => {
     return await City.find();
 }
+
+export const getCity = async (id) => {
+    return await City.findById(id);
+}
+
+export const createCity = async (data) => {
+    return City.create(data);
+}
+
+export const removeCity = async (id) => {
+    return City.findByIdAndDelete({_id: id});
+}
+
+export const updateCity = async (id, newData) => {
+    return City.findByIdAndUpdate({_id: id}, newData, {new: true});
+}
