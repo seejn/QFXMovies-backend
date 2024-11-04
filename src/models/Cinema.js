d47899521ee39d0bfea47d314483bfb037863544
seejn <seejnmaharjan@gmail.com>
2024-11-04 19:57:47 +0545

    modified cinema schema
    added [required] constraint on name
    added locatedIn reference to City

--- src/models/Cinema.js
+++ src/models/Cinema.js
@@ -1,7 +1,15 @@
 import mongoose, { Schema } from "mongoose";
 
 const schema = new Schema({
-    name: String
+    name: {
+        type: String,
+        required: true
+    },
+    locatedIn: {
+        type: Schema.Types.ObjectId,
+        ref: "City",
+        required: true
+    },
 });
 
 export default mongoose.model("Cinema", schema);
