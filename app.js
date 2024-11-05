b262b7709db7acd695c1f3184da2524c7527373c
seejn <seejnmaharjan@gmail.com>
2024-11-05 22:44:38 +0545

    registered datetimeroute and nowplayingmovieroute

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import { router as movieRoute } from "./src/routes/movie.route.js";
 import { router as cityRoute } from "./src/routes/city.route.js";
 import { router as cinemaRoute } from "./src/routes/cinema.route.js";
 import { router as genreRoute } from "./src/routes/genre.route.js";
+import { router as datetimeRoute } from "./src/routes/datetime.route.js";
+import { router as nowplayingmovieRoute } from "./src/routes/nowplayingmovie.route.js";
 
 dotenv.config();
 const app = express();
@@ -18,5 +20,7 @@ app.use("/api", movieRoute);
 app.use("/api", cityRoute);
 app.use("/api", cinemaRoute);
 app.use("/api", genreRoute);
+app.use("/api", datetimeRoute);
+app.use("/api", nowplayingmovieRoute);
 
 export default app;
