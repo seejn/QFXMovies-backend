1babbbafd21a847afcc91508f54a048057ece5e5
seejn <seejnmaharjan@gmail.com>
2024-11-06 18:51:45 +0545

    added token env varaibles

--- src/config/server.js
+++ src/config/server.js
@@ -1,1 +1,5 @@
 export const PORT = process.env.PORT
+export const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY
+export const ACCESS_EXPIRY = process.env.ACCESS_EXPIRY
+export const SECRET_REFRESH_KEY = process.env.SECRET_REFRESH_KEY
+export const REFRESH_EXPIRY = process.env.REFRESH_EXPIRY
