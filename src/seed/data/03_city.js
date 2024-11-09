e8a3c78e466533ed9047efe190e8a64ce9e13261
seejn <seejnmaharjan@gmail.com>
2024-11-09 18:50:01 +0545

    updated city data in seed files

--- src/seed/data/03_city.js
+++ src/seed/data/03_city.js
@@ -2,19 +2,19 @@ export default {
     model: "City",
     data: [
         {
-            "id": "672c1fa63f4eb09b99516349",
+            "_id": "672c1fa63f4eb09b99516349",
             "name": "Kathmandu"
         },
         {
-            "id": "672c1fa63f4eb09b9951634a",
+            "_id": "672c1fa63f4eb09b9951634a",
             "name": "Lalitpur"
         },
         {
-            "id": "672c1fa63f4eb09b9951634b",
+            "_id": "672c1fa63f4eb09b9951634b",
             "name": "Bhaktapur"
         },
         {
-            "id": "672c1fa63f4eb09b9951634c",
+            "_id": "672c1fa63f4eb09b9951634c",
             "name": "pokhara"
         }
     ]
