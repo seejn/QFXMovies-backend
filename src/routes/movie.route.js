3153b1e999beae4e0860b6480f5b105e703aa86a
seejn <seejnmaharjan@gmail.com>
2024-11-09 13:17:46 +0545

    added middleware for theatre, admin roleAuthorization in movie.route

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import * as movieController from "../controllers/MovieController.js";
+import { AuthJWT } from "../middlewares/AuthMiddleware.js";
+import { AuthorizedRoles } from "../middlewares/RoleAuthorization.js";
 
 export const router = Router();
 
 router.get("/movies", movieController.getAllMovies);
 router.get("/movies/:id", movieController.getMovieById);
-router.post("/movies", movieController.addMovie);
-router.patch("/movies/:id", movieController.updateMovie);
-router.delete("/movies/:id", movieController.deleteMovie);
+router.post("/movies", AuthJWT, AuthorizedRoles(["theatre"]), movieController.addMovie);
+router.patch("/movies/:id", AuthJWT, AuthorizedRoles(["theatre"]), movieController.updateMovie);
+router.delete("/movies/:id", AuthJWT, AuthorizedRoles(["theatre"]), movieController.deleteMovie);
