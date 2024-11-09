9d0fd993698d9af100bd44f24aef8de07685e05d
seejn <seejnmaharjan@gmail.com>
2024-11-09 13:36:14 +0545

    updated data in seeding files

--- src/seed/cinema.js
+++ src/seed/cinema.js
@@ -3,28 +3,40 @@ import seed from '../utils/seed.js';
 
 const cinemas = [
     {
+        _id: "672c1fab25641c4648230890",
         name: "Labim Mall",
-        locatedIn: "67287f6d0bb6a430e4dd7dce",
+        locatedIn: "672c1fa63f4eb09b99516349",
+        theatre: "672ef8ae55cc244bc4b7d4c8"
     },
     {
+        _id: "672c1fab25641c4648230891",
         name: "Civil Mall",
-        locatedIn: "67287f6d0bb6a430e4dd7dce",
+        locatedIn: "672c1fa63f4eb09b99516349",
+        theatre: "672ef8c755cc244bc4b7d4cd"
     },
     {
+        _id: "672c1fab25641c4648230892",
         name: "Chhaya Center",
-        locatedIn: "67287f6d0bb6a430e4dd7dce",
+        locatedIn: "672c1fa63f4eb09b99516349",
+        theatre: "672f068e55cc244bc4b7d4d2"
     },
     {
+        _id: "672c1fab25641c4648230893",
         name: "CG Mall",
-        locatedIn: "67287f6d0bb6a430e4dd7dd1"
+        locatedIn: "672c1fa63f4eb09b9951634c",
+        theatre: "672f07bc6686ffbc6545de4b"
     },
     {
+        _id: "672c1fab25641c4648230894",
         name: "Rising Mall",
-        locatedIn: "67287f6d0bb6a430e4dd7dd1"
+        locatedIn: "672c1fa63f4eb09b9951634c",
+        theatre: "672f07dd6686ffbc6545de52"
     },
     {
+        _id: "672c1fab25641c4648230895",
         name: "Trade Mall",
-        locatedIn: "67287f6d0bb6a430e4dd7dd1"
+        locatedIn: "672c1fa63f4eb09b9951634c",
+        theatre: "672f07ee6686ffbc6545de57"
     }
 ]
 
