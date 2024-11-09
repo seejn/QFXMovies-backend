04579bb64d0f160c6f39421c579bed5997ba4b1d
seejn <seejnmaharjan@gmail.com>
2024-11-09 18:50:16 +0545

    update nowplayingmovies data in seed file

--- src/seed/data/09_nowPlayingMovie.js
+++ src/seed/data/09_nowPlayingMovie.js
@@ -4,38 +4,26 @@ export default {
         {
             cinema: "672c1fab25641c4648230890",
             movie: "67297133cab55d3b5fdc3ec1",
-            date: "6729f7dd2795f271ba90a90c",
-            time: "672a39f7e6db80607cddbad6"
         },
         {
             cinema: "672c1fab25641c4648230893",
             movie: "67297133cab55d3b5fdc3ec1",
-            date: "6729f7dd2795f271ba90a90c",
-            time: "672a39f7e6db80607cddbad8"
         },
         {
             cinema: "672c1fab25641c4648230892",
             movie: "67297133cab55d3b5fdc3ec1",
-            date: "6729f7dd2795f271ba90a90c",
-            time: "672a39f7e6db80607cddbada"
         },
         {
             cinema: "672c1fab25641c4648230890",
             movie: "67297133cab55d3b5fdc3ec2",
-            date: "6729f7dd2795f271ba90a90c",
-            time: "672a39f7e6db80607cddbad6"
         },
         {
             cinema: "672c1fab25641c4648230893",
             movie: "67297133cab55d3b5fdc3ec2",
-            date: "6729f7dd2795f271ba90a90c",
-            time: "672a39f7e6db80607cddbad8"
         },
         {
             cinema: "672c1fab25641c4648230892",
             movie: "67297133cab55d3b5fdc3ec2",
-            date: "6729f7dd2795f271ba90a90c",
-            time: "672a39f7e6db80607cddbada"
         },
     ]
 };
